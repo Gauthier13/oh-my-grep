2bd2f727dd60be39bded74b3f9ab26cecede1ff3
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-03 21:01:22 +0100

    add test to search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
-pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
