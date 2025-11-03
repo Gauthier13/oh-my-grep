0e717e17e4f8645284638e576fd5dcaf1cd28a27
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-03 19:39:43 +0100

    refacto: add run function, add error handling for the build fn of Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-use std::{env, fs}; // Apporte dans le scope le module env
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // Permet de passer des arguments à cargo run
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    // Permet de lire le fichier passé en argument.
-    // Expect est du type Result<T,E> et la string passé en argument permet de gérer l'erreur si le
-    // fichier passé n'existe pas
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    run(config)
 }
 
 struct Config {
@@ -22,15 +19,28 @@ struct Config {
     file_path: String,
 }
 
-/**
-*@description Parse the config from user input
-*@return a tuple
-*/
 impl Config {
-    fn new(args: &[String]) -> Config {
+    /**
+     *@description Parse the config from user input
+     */
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Missing arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    // Permet de lire le fichier passé en argument.
+    // Expect est du type Result<T,E> et la string passé en argument permet de gérer l'erreur si le
+    // fichier passé n'existe pas
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
