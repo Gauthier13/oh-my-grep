e1ba2c29f6e42016ab1de086c48537ccc8335182
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-03 19:27:15 +0100

    impl Config struct with a function to construct config values from user input

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs}; // Apporte dans le scope le module env
 fn main() {
     let args: Vec<String> = env::args().collect(); // Permet de passer des arguments à cargo run
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -26,9 +26,11 @@ struct Config {
 *@description Parse the config from user input
 *@return a tuple
 */
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
