c64c4a78960f5ad71f55416fea56f1d3ec10b820
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-03 19:03:07 +0100

    read the file passed as argument to the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env; // Apporte dans le scope le module env
+use std::{env, fs}; // Apporte dans le scope le module env
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // Permet de passer des arguments à cargo run
@@ -8,4 +8,11 @@ fn main() {
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    // Permet de lire le fichier passé en argument.
+    // Expect est du type Result<T,E> et la string passé en argument permet de gérer l'erreur si le
+    // fichier passé n'existe pas
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
