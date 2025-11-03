c8e1264b7320ac29ac24dc1a95984b087b6290e7
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-03 18:54:58 +0100

    saving inputs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::env;
+use std::env; // Apporte dans le scope le module env
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let args: Vec<String> = env::args().collect(); // Permet de passer des arguments à cargo run
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
