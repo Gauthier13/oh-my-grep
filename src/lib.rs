ec15ba583c54512d297f684517fa10e6668a5e33
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-04 20:38:57 +0100

    add case insensitve test and function declaration

--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,38 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-pick three.";
+pick three
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three
+Trust me.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
