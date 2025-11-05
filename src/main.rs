1d7d6731fb489b8d775ec3465cafc3f51b3f0aeb
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-05 20:34:11 +0100

    use returned value of iterator to build the config

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::{env, fs, process};
 use ohMyGrep::{search, search_case_insensitive};
 
 fn main() {
-    let args: Vec<String> = env::args().collect(); // Permet de récupérer les arguments rentrés en ligne de commande
+    // get the arguments from the command line input
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -20,6 +20,7 @@ fn main() {
     }
 }
 
+// this are the three arguments a user can use for running the grep function
 struct Config {
     pub query: String,
     pub file_path: String,
@@ -31,13 +32,20 @@ impl Config {
     @description Parse the config from user input
     @error return an error if inputs are missings
     */
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Missing arguments");
-        }
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // the first argument returned by env::args is the name of the programm so we ignore it
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a valide file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
